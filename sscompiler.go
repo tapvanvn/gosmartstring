a13e44465f6590cc1b7cc0a11f386842f3a93bd5
duy.nguyen <tapvanvn@gmail.com>
2021-03-24 13:18:04 +0700

    update

--- sscompiler.go
+++ sscompiler.go
@@ -102,6 +102,7 @@ func (compiler *SSCompiler) compileDo(token *gotokenize.Token, context *SSContex
 
 				params = append(params, param.Object)
 			} else {
+				fmt.Println("registry not found " + childToken.Content)
 				return errors.New("registry not found " + childToken.Content)
 			}
 		}
