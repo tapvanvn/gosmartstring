743786fa02ffc51fbca24647901a1255f4b0ba74
Duy Nguyen <tapvanvn@gmail.com>
2022-05-02 11:49:08 +0700

    print debug

--- smarstring_high_meaning.go
+++ smarstring_high_meaning.go
@@ -1,12 +1,13 @@
 package gosmartstring
 
 import (
+	"fmt"
+
 	"github.com/tapvanvn/gotokenize/v2"
 )
 
 type SmarstringMeaning struct {
 	*gotokenize.AbstractMeaning
-	//gotokenize.PatternMeaning
 }
 
 func CreateSSMeaning() *SmarstringMeaning {
@@ -22,11 +23,11 @@ func (meaning *SmarstringMeaning) Prepare(process *gotokenize.MeaningProcess) {
 
 	meaning.AbstractMeaning.Prepare(process)
 
-	//fmt.Println("--1.0--")
-	//process.Stream.Debug(0, SSNaming, &gotokenize.DebugOption{
-	//	ExtendTypeSize: 6,
-	//})
-	//fmt.Println("--end 1.0--")
+	fmt.Println("--1.0--")
+	process.Stream.Debug(0, SSNaming, &gotokenize.DebugOption{
+		ExtendTypeSize: 6,
+	})
+	fmt.Println("--end 1.0--")
 
 	tmpStream := gotokenize.CreateStream(0)
 
